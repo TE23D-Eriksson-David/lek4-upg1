435a20dc8cf137c25f7b69e3c5ddd9e1e90a6cff
TE23D-Eriksson-David <david.eriksson2@elev.ga.ntig.se>
2025-09-30 12:18:19 +0200

    Klar

--- src/App.java
+++ src/App.java
@@ -1,13 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
         Scanner TB = new Scanner(System.in);
         Klass klass = new Klass();
+        boolean klar = false;
+        int val = 0;
 
         while (true) {
-            klass.läggTillElev(registreraElev(TB));
-            klass.visaKlass();
+            System.out.println("\n1. Lägg Till Elev\n2. Hitta elevinformation\n3. Visa klass");
+            System.out.print(":");
+            val = VALMETOD(klar, val, TB, 1, 3);
+            switch (val) {
+                case 1:
+                    klass.läggTillElev(registreraElev(TB));
+                    break;
+
+                case 2:
+                    klass.hittaElevInfo(TB);
+                    break;
+
+                case 3:
+                klass.visaKlass();
+                    break;
+            }
+
         }
     }
 
@@ -17,6 +36,8 @@ public class App {
         System.out.println("1. Namn\n2. Person Numer\n3. Email\n4. Mobilnumer\n5. Klass\n6. Program\n");
 
         System.out.println("Namn:");
+                if (TB.hasNext())
+            TB.nextLine();
         String namnElev = TB.nextLine();
 
         System.out.println("Personumer:");
@@ -43,4 +64,31 @@ public class App {
         Elever elev = new Elever(namnElev, personNumer, email, mobilNumer, klass, program);
         return elev;
     }
+
+    static int VALMETOD(boolean klar, int val, Scanner TB, int Minstval, int Störstval) {
+    klar = false;
+    val = 0;
+    while (klar == false) {
+        try {
+            val = TB.nextInt();
+            klar = true;
+            if (val < Minstval || val > Störstval) {
+                klar = false;
+                System.out.println("Ange en sifra som korispoderar till en av valen!");
+                System.out.print("Ange: ");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Skriv bara in sifror!");
+            System.out.print("Ange: ");
+            klar = false;
+            if (TB.hasNext())
+                TB.next();
+        }
+    }
+    klar = false;
+    return val;
 }
+
+    
+}
+
--- src/Klass.java
+++ src/Klass.java
@@ -1,18 +1,41 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Klass {
     ArrayList<Elever> Klassen = new ArrayList<Elever>();
 
-    public void läggTillElev(Elever elev){
+    public void läggTillElev(Elever elev) {
         Klassen.add(elev);
     }
 
-    public void visaKlass(){
+    public void visaKlass() {
         for (int i = 0; i < Klassen.size(); i++) {
             Elever elev = Klassen.get(i);
             System.out.println();
-            System.out.println("Namn: "+elev.namn+"\nPersonumer: "+elev.personNumer+"\nEmail: "+elev.email+"\nMobilnumer: "+elev.mobilNumer+"\nKlass: "+elev.klass+"\nProgram: "+elev.program+"");
+            System.out.println("Namn: " + elev.namn + "\nPersonumer: " + elev.personNumer + "\nEmail: " + elev.email
+                    + "\nMobilnumer: " + elev.mobilNumer + "\nKlass: " + elev.klass + "\nProgram: " + elev.program
+                    + "");
         }
-        
+
+    }
+
+    public void hittaElevInfo(Scanner TB) {
+        System.out.println("\nAnge namnet för eleven: ");
+        if (TB.hasNext())
+            TB.nextLine();
+        String namn = TB.nextLine();
+
+        for (int i = 0; i < Klassen.size(); i++) {
+            Elever elev = Klassen.get(i);
+            if (namn.equals(elev.namn)) {
+                System.out.println("\nNamn: " + elev.namn + "\nPersonumer: " + elev.personNumer + "\nEmail: " + elev.email
+                        + "\nMobilnumer: " + elev.mobilNumer + "\nKlass: " + elev.klass + "\nProgram: " + elev.program
+                        + "");
+                break;
+            }
+            if (i == Klassen.size()) {
+                System.out.println("Namnet som du har anget verkar inte tillhöra någon elev.\nFörsök igen,");
+            }
+        }
     }
 }
